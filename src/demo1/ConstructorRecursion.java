1c23874e460808e26d6a728979c57602d4d00bd1
tyss <tyss@localhost>
2017-11-09 13:58:34 +0530

    Hi i am pushing the fourth project DEmo1 into the git hub

---
+++ src/demo1/ConstructorRecursion.java
@@ -0,0 +1,19 @@
+package demo1;
+ 
+public class ConstructorRecursion{
+ 
+	//ConstructorRecursion b= new ConstructorRecursion();
+	int age;
+ 
+ public int show(){
+      return (true ? null : 0);
+ }
+ 
+ public static void main(String[] args)  {
+
+	 ConstructorRecursion b= new ConstructorRecursion();
+        b.show();
+    }
+ 
+}
+                                 
